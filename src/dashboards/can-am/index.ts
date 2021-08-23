1c5e34e4e4d482a0ead5e8d4be93087d7d695b15
Paresh Rathod <paresh.rathod@awanoo.com>
2021-08-23 16:12:52 +0530

    solved productline issue and extra product removed from list

--- src/dashboards/can-am/index.ts
+++ src/dashboards/can-am/index.ts
@@ -26,15 +26,13 @@ export class CanAmDashboard implements ManufacturerInterface {
   private processData: boolean = false;
   public imgUrl: any;
   private cookieFile: string = "";
-  
+
   constructor() {
     this.cookieFile = COOKIE_PATH + "canam-cookie.json";
   }
   public async crawl(partInfos: [types.OEMPartInfo]) {
     this.arr = partInfos;
-    await this.initialize(
-
-    );
+    await this.initialize();
     await this.login(USER_NAME, PASSWORD, DEALERNUMBER);
     await this.inquiry(this.arr);
     return this.data;
@@ -43,9 +41,7 @@ export class CanAmDashboard implements ManufacturerInterface {
     try {
       this.browser = await puppeteer.launch({
         headless: false,
-        args: [
-          "--no-sandbox",
-          "--disable-dev-shm-usage"],
+        args: ["--no-sandbox", "--disable-dev-shm-usage"],
       });
 
       this.page = await this.browser.newPage();
@@ -83,76 +79,80 @@ export class CanAmDashboard implements ManufacturerInterface {
     try {
       var data = new Array();
       let start = +new Date();
+      
       for (let d = 0; d < arr.length; d++) {
         await this.page.goto(ITEM_INQUIRY_URL, { waitUntil: "domcontentloaded" });
+        // await this.page.goto("http://localhost/parts.html", {
+        //   waitUntil: "domcontentloaded",
+        // });
         await this.page.waitForTimeout(3000);
 
         await this.page.select(
-          "select#PlaceHolderMain_SelectProductLine",
-          arr[d].productLine
+          "#PlaceHolderMain_SelectProductLine",
+          arr[d].productLine.toString()
         );
         //await this.page.waitForTimeout(2000);
 
         await this.page.type(
           'input[id="PlaceHolderMain_PartEntryZone_PartNumber_0"]',
-          arr[d].partNumber
+          arr[d].partNumber, 100
         );
         await this.page.type(
           'input[id="PlaceHolderMain_PartEntryZone_RequestedQuantity_0"]',
-          arr[d].requestedQty
+          arr[d].requestedQty.toString(), 100
         );
 
         let nextButton = await this.page.$x(
           '//*[@id="CommandBar1_SubmitAction_ActionButton"]'
         );
         await nextButton[0].click();
-        // await this.page.waitForSelector('#formZone > div > table > tbody > tr > td > table > tbody > tr:nth-child(1) > th:nth-child(2)');
-
-        //await this.page.waitForTimeout(6000);
-
-        let create_data = await this.page.evaluate(() => {
-          var json = {};
-
+               
+        await this.page.waitForTimeout(6000);
+        
+        let create_data = await this.page.evaluate(({arr, d}) => {
           let key_array = new Array();
           let val_array = new Array();
-          let key_query = document
-            .querySelectorAll(
-              "#formZone > div > table > tbody > tr > td > table > tbody > tr"
-            )[0]
-            .querySelectorAll("th");
+          let jsonArray:any = new Array();
+          const rows = document.querySelectorAll(
+            "table.SectionTable > tbody > tr > td:nth-child(1) > table > tbody > tr"
+          );
+          let key_query = rows[0].querySelectorAll("th");
           for (let i = 0; i < key_query.length; i++) {
             const key_element = key_query[i].innerText;
             key_array.push(
               key_element.replace(/\r?\n|\r/g, "").replace(/ /g, "")
             );
           }
-          let val_query = document
-            .querySelectorAll(
-              "#formZone > div > table > tbody > tr > td > table > tbody > tr"
-            )[1]
-            .querySelectorAll("td");
-          for (let i = 0; i < val_query.length; i++) {
-            const val_element = val_query[i].innerText;
-            val_array.push(
-              val_element.replace(/\r?\n|\r/g, "").replace(/ /g, "")
-            );
+
+          let jsonObject: any;
+          
+          for (let i = 1; i < rows.length; i++) {
+            jsonObject = {};
+            const tds = rows[i].querySelectorAll("td");
+            for (let j = 0; j < tds.length; j++) {
+              const val_element = tds[j].innerText;
+              val_array.push(
+                val_element.replace(/\r?\n|\r/g, "").replace(/ /g, "")
+              );
+              jsonObject[key_array[j]] = val_element.replace(/\r?\n|\r/g, "").replace(/ /g, "");
+            }
+            jsonArray.push(jsonObject);
           }
-
-          for (let k = 0; k < key_array.length; k++) {
-            json[key_array[k]] = val_array[k];
-          }
-
-          return json;
-        });
-
+          console.log("jsonArray==>", jsonArray);
+          const result = jsonArray.filter(item => item['PartNumber'] === arr[d].partNumber.toString()  && item['Ex.'] !== '*');
+          console.log("result==>", result);
+          return result[0];
+        }, { arr, d});
+        console.log("create_data==>", create_data)
         data.push(create_data);
       }
+      
       console.log("create_data====", data);
       this.data = data;
       let Total_time = +new Date() - start;
       console.log("Total Process Time in milliseconds", Total_time);
       console.log("Process Completed Successfully");
-      await this.browser.close();
+      //await this.browser.close();
     } catch (error) {
       console.log("inquiry Error==>", error);
 
@@ -167,12 +167,29 @@ export class CanAmDashboard implements ManufacturerInterface {
   ) {
     await this.page.goto(BASE_URL);
     await this.page.waitForTimeout(2000);
+    const selector = 'input[name="j_id0:j_id5:Login1_Dealer_No"]';
+    await this.page.evaluate((selector) => {
+      document.querySelector(selector).value = "";
+    }, selector);
     await this.page.type(
       'input[name="j_id0:j_id5:Login1_Dealer_No"]',
-      dealrnumber, 100
+      dealrnumber,
+      100
     );
-    await this.page.type('input[name="j_id0:j_id5:Login1_UserName"]', username, 100);
-    await this.page.type('input[name="j_id0:j_id5:Login1_Password"]', password, 100);
+    const selUser = 'input[name="j_id0:j_id5:Login1_UserName"]';
+    await this.page.evaluate((selUser) => {
+      document.querySelector(selUser).value = "";
+    }, selUser);
+    await this.page.type(
+      'input[name="j_id0:j_id5:Login1_UserName"]',
+      username,
+      100
+    );
+    await this.page.type(
+      'input[name="j_id0:j_id5:Login1_Password"]',
+      password,
+      100
+    );
 
     let signInButton = await this.page.$x('//*[@id="j_id0:j_id5:j_id24"]');
     await Promise.all([this.page.waitForNavigation(), signInButton[0].click()]);
